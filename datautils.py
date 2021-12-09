71f3ba16826dcb56ee6629558866308a3f7430c3
Pooria90 <61358399+Pooria90@users.noreply.github.com>
2021-12-09 13:38:03 +0330

    some modifications in datautil; adding main

--- datautils.py
+++ datautils.py
@@ -12,7 +12,7 @@ from scipy.io import loadmat
 from pathlib import Path
 
 
-def butter_bandpass (lowcut, highcut, fs, order=5):
+def butter_bandpass (lowcut, highcut, fs, order=6):
     nyq = 0.5 * fs
     low = lowcut / nyq
     high = highcut / nyq
@@ -20,7 +20,7 @@ def butter_bandpass (lowcut, highcut, fs, order=5):
     return b, a
 
 
-def butter_bandpass_filter (signal, lowcut, highcut, fs, order=5):
+def butter_bandpass_filter (signal, lowcut, highcut, fs, order=6):
     b, a = butter_bandpass(lowcut, highcut, fs, order=order)
     y = lfilter(b, a, signal)
     return y
@@ -46,7 +46,7 @@ def eog_remove (signal, eeg_ch=22, eog_ch=3):
     for i in range(eog_ch):
         for j in range(eeg_ch):
             cov   = np.cov(N[i:i+1, :], Y[j:j+1, :])
-            covNY = cov[0, 1]
+            covNY[i,j] = cov[0, 1]
     
     b = np.linalg.inv(autoN).dot(covNY)
     return b
@@ -63,23 +63,36 @@ def downloader (path, subjects, url="http://bnci-horizon-2020.eu/database/data-s
     '''
     
     data_path = Path(path)
+    data_path.mkdir(parents=True, exist_ok=True)
     for sj in subjects:
           for L in ['E','T']:
                 filename = "A0" + str(sj) + L + '.mat'
                 if not (data_path / filename).exists():
                     content = requests.get(url + filename).content
                     (data_path / filename).open("wb").write(content)
+    print (f'Subjects {np.array(subjects)} are now in {data_path}') 
     return data_path
 
 
-def mat_extractor (path, beg=500, end=1500, remove_eog = True):
+def mat_extractor (
+    path, beg = 500, end = 1500,
+    remove_eog = True, bpf_dict={'apply':True, 'fs': 250, 'lc':4, 'hc':38, 'order':6},
+    channel_norm = True
+    ):
     '''
-    function for extracting data from a single mat file
+    function for extracting data from a single mat file and doing preprocessing on it
     
-    data_path  : the directroy of the desired .mat file
-    beg, end   : refer to the begining and end og the part of the 8-second 
-                 signal that the motor imagery task is done
-    remove_eog : decides whether we want to remove EOG or use raw EEG
+    data_path    : the directroy of the desired .mat file
+    beg, end     : refer to the begining and end og the part of the 8-second 
+                   signal that the motor imagery task is done
+    remove_eog   : decides whether we want to remove EOG or use raw EEG
+    bpf_dict     : a dictionary for specifications of bandpass filter that we want to apply
+                   it should be defined with the following keys and values
+                   'apply'   : deciding whether we want to apply bpf or not (boolean)
+                   'lc','hc' : lowcut and highcut of butterworth bpf (float)
+                   'fs'      : sampling rate (250 for bcic-IV-2a)
+                   'order'   : the order of butterworth bpf
+    channel_norm : z-score normalization for each channel (boolean)
     '''
     
     mat  = loadmat(path)
@@ -94,7 +107,7 @@ def mat_extractor (path, beg=500, end=1500, remove_eog = True):
         opened = data[0][0][0][0][0]
         closed = data[0][1][0][0][0]
         motion = data[0][2][0][0][0]
-        allsig = np.concatenate((opened, closed, motion),axis=0).T
+        allsig = np.concatenate((opened, closed, motion),axis=0)
         b = eog_remove(allsig)
     
     # iteration on motor imagery task sessions
@@ -115,13 +128,52 @@ def mat_extractor (path, beg=500, end=1500, remove_eog = True):
             else:
                 x = x[beg: end, 0: -3]
             
-            x = x.reshape(1, x.shape[0], x.shape[1])
+            if bpf_dict['apply']:
+                x = butter_bandpass_filter(
+                    signal  = x,
+                    lowcut  = bpf_dict['lc'],
+                    highcut = bpf_dict['hc'],
+                    fs      = bpf_dict['fs'],
+                    order   = bpf_dict['order']
+                )
+                
+            if channel_norm:
+                x = (x - np.mean(x))/np.std(x)
+            
+            x = np.expand_dims(x, axis=0)
             y = np.array([labels[i,0]])
             
             xx = np.concatenate((xx, x),axis=0)
             yy = np.concatenate((yy, y),axis=0)
     
-    return xx, yy
+    return xx[1:], yy[1:] # first sample is empty
+
+
+def cropper (signals, labels, window, step):
+    '''
+    function to crop signals with a window and step for data augmentation
+    
+    signals : in format (N, ch, T) where N is the number of samples,
+              ch is the number of channels, and T is the number of time points
+    labels  : labels of samples
+    window  : the window for cropping signals in terms of time points (int)
+    step    : the step for moving the window in term of time points (int)
+    '''
+    
+    time   = signals.shape[2]             # number of time points
+    begs   = list(range(0, time-1, step)) # begining indices of sliding windows
+    crops  = list()                       # list contaiing croppend signals
+    annots = list()                       # labels of cropped signals
+    
+    for i in range(signals.shape[0]):
+        for j in begs:
+            crops .append(signals[i:i+1, :, j:j+window])
+            annots.append(labels[i])
+    
+    crops  = np.concatenate(crops, axis=0)
+    annots = np.array(annots)
+    
+    return crops, annots
 
 
 def dataset_extractor (train_subs, test_subs, data_path):
